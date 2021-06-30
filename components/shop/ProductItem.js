8dfaa9498f0130556c0bd43f260bc4984248dbf1
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-06-30 18:17:46 +0530

    Product Deatils Screen & Custom Fonts Done

--- components/shop/ProductItem.js
+++ components/shop/ProductItem.js
@@ -62,10 +62,12 @@ const styles=StyleSheet.create({
     title:{
         fontSize:18,
         marginVertical:4,
+        fontFamily:'open-sans-bold',
     },
     price:{
         fontSize:14,
         color:"#888",
+        fontFamily:'open-sans',
     },
     actions:{
         flexDirection:'row',
--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { StyleSheet,View,Text } from 'react-native';
+import { StyleSheet,Image,View,Button,Text, ScrollView, Dimensions } from 'react-native';
 import { useSelector } from 'react-redux';
+import Colors from '../../constants/Colors';
 
 
 const ProductDetailScreen=props=>{
     const productId=props.navigation.getParam('productId');
     const selectedProduct=useSelector(state=>state.products.availableProducts.find(prod=>prod.id===productId));
-    
+    console.log(selectedProduct.imageUrl);
     return(
-        <View>
-            <Text>{selectedProduct.title}</Text>
-        </View>
+        <ScrollView>
+            <View style={styles.imageView}>
+                <Image style={styles.image} source={{uri:selectedProduct.imageUrl}}/>
+            </View>
+            <View style={styles.actions}>
+                <Button color={Colors.primary} title="Add To Cart" onPress={()=>{}}/>
+            </View>
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}> {selectedProduct.description}</Text>
+        </ScrollView>
     );
 };
 
@@ -22,7 +30,31 @@ ProductDetailScreen.navigationOptions=navData=>{
 }
 
 const styles=StyleSheet.create({
-
+    imageView:{
+        width:"100%",
+        height:Dimensions.get('window').height*0.4,
+    },
+    image:{
+       width:"100%",
+       height:"100%",
+    },
+    price:{
+        fontSize:20,
+        color:"#888",
+        textAlign:'center',
+        marginVertical:20,
+        fontFamily:'open-sans-bold',
+    },
+    description:{
+        fontSize:14,
+        textAlign:'center',
+        marginHorizontal:20,
+        fontFamily:'open-sans',
+    },
+    actions:{
+        marginVertical:10,
+        alignItems:'center',
+    }
 });
 
 export default ProductDetailScreen;
