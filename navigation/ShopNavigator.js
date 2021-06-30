32b859e503d815aaedeeb12302343a3f44f81e9d
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-07-01 04:10:15 +0530

    SideDrawer

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack';
 import { Platform } from 'react-native';
 import Colors from '../constants/Colors';
@@ -5,7 +6,23 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import { createAppContainer } from 'react-navigation';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { Ionicons } from '@expo/vector-icons';
 
+const defaultNavOptions={
+    headerStyle:{
+        backgroundColor: Platform.OS==='android' ? Colors.primary :'',
+    },
+    headerTitleStyle:{
+        fontFamily:'open-sans-bold',
+    },
+    headerBackTitleStyle:{
+        fontFamily:'open-sans',
+    },
+    headerTintColor:Platform.OS==='android' ? 'white' : Colors.primary,
+
+};
 
 const ProductsNavigator=createStackNavigator({
     ProductsOverview:ProductsOverviewScreen,
@@ -13,21 +30,38 @@ const ProductsNavigator=createStackNavigator({
     Cart:CartScreen,
 },
 {
-    defaultNavigationOptions:{
-        headerStyle:{
-            backgroundColor: Platform.OS==='android' ? Colors.primary :'',
-        },
-        headerTitleStyle:{
-            fontFamily:'open-sans-bold',
-        },
-        headerBackTitleStyle:{
-            fontFamily:'open-sans',
-        },
-        headerTintColor:Platform.OS==='android' ? 'white' : Colors.primary,
-
-    }
+    navigationOptions:{
+        drawerIcon:drawerConfig=>(
+            <Ionicons name={Platform.OS==='android' ? 'md-cart' :'ios-cart'}
+                size={23} color={drawerConfig.tintColor}
+            />
+        ),
+    },
+    defaultNavigationOptions:defaultNavOptions,
 });
 
+const OrdersNavigator=createStackNavigator({
+    Orders:OrdersScreen,
+},
+{
+    navigationOptions:{
+        drawerIcon:drawerConfig=>(
+            <Ionicons name={Platform.OS==='android' ? 'md-list' :'ios-list'}
+                size={23} color={drawerConfig.tintColor}
+            />
+        ),
+    },
+    defaultNavigationOptions:defaultNavOptions,
+});
 
+const ShopNavigator=createDrawerNavigator({
+    Products:ProductsNavigator,
+    Orders:OrdersNavigator,
+},
+{
+    contentOptions:{
+        activeTintColor:Colors.primary,
+    }
+})
 
-export default createAppContainer(ProductsNavigator);
+export default createAppContainer(ShopNavigator);
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -20,6 +20,15 @@ const ProductsOverviewScreen=props=>{
 ProductsOverviewScreen.navigationOptions=navData=>{
     return({
         headerTitle:'All Products',
+        headerLeft:()=>(
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item title="Menu" iconName={Platform.OS==='android'?"md-menu":'ios-menu'} 
+                    onPress={()=>{
+                        navData.navigation.toggleDrawer();
+                    }}
+                />
+            </HeaderButtons>
+        ),
         headerRight:()=>(
             <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
                 <Item title="Cart" iconName={Platform.OS==='android'?"md-cart":'ios-cart'} 
