97c8bcb2c167329e0f878a35dd2d7b3696a91bd2
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-07-01 04:17:06 +0530

    bug fix and clearing the cart

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { HeaderButtons,Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from '../../components/UI/HeaderButton';
 const OrdersScreen=props=>{
-    const orders=useSelector(state=>state.orders);
+    const orders=useSelector(state=>state.orders.orders);
     return (
         <FlatList
             data={orders} keyExtractor={(item,idx)=>item.id}
