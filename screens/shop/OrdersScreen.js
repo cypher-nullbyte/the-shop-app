466bfc27de1cb4c3891ba2f63a73ec11054e16ed
cYpHeR <64411912+cypher-nullbyte@users.noreply.github.com>
2021-07-01 11:21:18 +0530

    Styling Order Items

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -3,12 +3,18 @@ import { Text,FlatList } from 'react-native';
 import { useSelector } from 'react-redux';
 import { HeaderButtons,Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from '../../components/UI/HeaderButton';
+import OrderItem from '../../components/shop/OrderItem';
+
 const OrdersScreen=props=>{
     const orders=useSelector(state=>state.orders.orders);
     return (
         <FlatList
             data={orders} keyExtractor={(item,idx)=>item.id}
-            renderItem={itemData=><Text>{itemData.item.totalAmount}</Text>}
+            renderItem={itemData=>(
+                <OrderItem 
+                    amount={itemData.item.totalAmount} date={itemData.item.readableDate}
+                />
+            )}
         />
     );
 };
